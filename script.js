bb0e142e46efadfb58fc4900aeba3393a7ab9e37
maxKILLer0666 <maxim8777000@gmail.com>
2026-04-07 18:00:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//task 1
+console.error("task 1")
+let a = 10
+let b = 5
+let c = 2
+
+console.log(a + c)
+console.log(a * b / c)
+console.log(a % b)
+
+//task 2
+console.error("task 2")
+let x = 7
+
+console.log(x++ + ++x)
+console.log(x-- - --x)
+
+//task 3
+console.error("task 3")
+let pon = 4
+let on = 5
+let str = "The result is: "
+
+console.log(str + pon + on)
+
+//task 4
+console.error("task 4")
+/*1*/console.log(a > b, a > 0 && b > 0)
+/*2*/console.log(c < 10 || c == 2)
+/*3*/console.log(a / c != 5)
+
+//task 5
+console.error("task 5")
+let ladno = 10
+let gavno = "10"
+
+console.log(ladno == gavno) //проверяет значение несмотря на типы переменных
+console.log(ladno === gavno) //проверяет значение считая и типы переменных
+
+//task 6
+console.error("task 6")
+let x_x = 21
+let pravda = (x_x / 2 || x_x / 3 && x_x % 6)
+
+console.log(!!pravda)
+let y = 11
+let lozh = (y > 10 || y < 5)
+
+console.log(!lozh)
